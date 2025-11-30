7403c2dadc4ae0711e8aae2acd3bbf900a8df885
MircaMircas <mikosmikus@gmail.com>
2025-11-30 21:29:11 +0100

    Expanding the sound index, part 2
    Added conditional compilation for MoreSounds feature in sound selection.

--- sonic.asm
+++ sonic.asm
@@ -2367,7 +2367,9 @@ LevelSelect:
 		cmpi.w	#$14,d0		; have you selected item $14 (sound test)?
 		bne.s	LevSel_Level_SS	; if not, go to Level/SS subroutine
 		move.w	(v_levselsound).w,d0
+	if ~~MoreSounds
 		addi.w	#$80,d0
+	endif
 		tst.b	(f_creditscheat).w ; is Japanese Credits cheat on?
 		beq.s	LevSel_NoCheat	; if not, branch
 		cmpi.w	#$9F,d0		; is sound $9F being played?
@@ -2622,16 +2624,20 @@ LevSel_SndTest:
 		btst	#bitL,d1	; is left pressed?
 		beq.s	LevSel_Right	; if not, branch
 		subq.w	#1,d0		; subtract 1 from sound test
+	if ~~MoreSounds
 		bhs.s	LevSel_Right
 		moveq	#$4F,d0		; if sound test moves below 0, set to $4F
+	endif
 
 LevSel_Right:
 		btst	#bitR,d1	; is right pressed?
 		beq.s	LevSel_Refresh2	; if not, branch
 		addq.w	#1,d0		; add 1 to sound test
+	if ~~MoreSounds
 		cmpi.w	#$50,d0
 		blo.s	LevSel_Refresh2
 		moveq	#0,d0		; if sound test moves above $4F, set to 0
+	endif
 
 LevSel_Refresh2:
 		move.w	d0,(v_levselsound).w ; set sound test number
@@ -2689,7 +2695,9 @@ LevSel_DrawAll:
 LevSel_DrawSnd:
 		locVRAM	vram_bg+$C30		; sound test position on screen
 		move.w	(v_levselsound).w,d0
+	if ~~MoreSounds
 		addi.w	#$80,d0
+	endif
 		move.b	d0,d2
 		lsr.b	#4,d0
 		bsr.w	LevSel_ChgSnd	; draw 1st digit
